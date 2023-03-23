4fcff15d31833af9b864e5dc6b2600c101f95c6d
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-23 01:08:57 -0300

    test: Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,21 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
 
 
 def test_encrypt_message():
-    pass
+
+    with raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("password", "0")
+
+    with raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(235813, 0)
+   
+    invalid_index = encrypt_message("password", -1)
+    even = encrypt_message("password", 2)
+    odd = encrypt_message("password", 3)
+
+    assert invalid_index == "drowssap"
+    assert even == "drowss_ap"
+    assert odd == "sap_drows"
+
+    
