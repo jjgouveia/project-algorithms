beb65e3be127ef0ec792fb36c19ba8b0e11dd4aa
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-22 23:35:35 -0300

    feat: Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    n = 0
+    period = range(len(permanence_period))
+    try:
+        for i in period:
+            entry, exit = permanence_period[i]
+            if entry <= target_time <= exit:
+                n += 1
+        return n
+    except TypeError:
+        None
+
