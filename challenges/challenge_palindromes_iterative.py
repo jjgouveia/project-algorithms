ac66a4503c6b0c353e47369155eabebfca391daa
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-24 00:31:04 -0300

    refactor: Requisito 6: adicionado type hint e diminuida a complexidade de espaço

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,9 +1,8 @@
-def is_palindrome_iterative(word):
+def is_palindrome_iterative(word: str):
     if not word:
         return False
 
-    word = word.lower()
-    word = word.replace(" ", "")
+    word = word.lower().replace(" ", "")
 
     reversed_word = word[::-1]
 
